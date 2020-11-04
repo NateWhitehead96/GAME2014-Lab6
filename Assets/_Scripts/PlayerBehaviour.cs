37e59ab0386937174ea456ea483a42304b8f70bc
NateWhitehead96 <n_whitehead96@hotmail.ca>
2020-11-04 14:27:59 -0500

    Lab6
    Following along in class to create a starter platformer for mobile

---
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public Joystick joystick;
+    public float joystickHorizontalSensitivity;
+    public float joystickVerticalSensitivity;
+    public float horizontalForce;
+    public float verticalForce;
+    public bool grounded;
+    public GameObject spawnPoint;
+
+    private Rigidbody2D m_rb2d;
+    private SpriteRenderer m_spriteRenderer;
+    private Animator m_animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb2d = GetComponent<Rigidbody2D>();
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+        m_animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+    }
+
+    void _Move()
+    {
+        if(grounded)
+        {
+            if (joystick.Horizontal > joystickHorizontalSensitivity)
+            {
+                // move right
+                m_rb2d.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
+                m_spriteRenderer.flipX = false;
+                m_animator.SetInteger("AnimState", 1);
+            }
+            else if (joystick.Horizontal < -joystickHorizontalSensitivity)
+            {
+                // move left
+                m_rb2d.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
+                m_spriteRenderer.flipX = true;
+                m_animator.SetInteger("AnimState", 1);
+            }
+            else if (joystick.Vertical > joystickVerticalSensitivity)
+            {
+                // jump
+                m_rb2d.AddForce(Vector2.up * verticalForce * Time.deltaTime);
+                m_animator.SetInteger("AnimState", 2);
+            }
+            else
+            {
+                //idle
+                m_animator.SetInteger("AnimState", 0);
+            }
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        grounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        grounded = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // respawn
+        if(collision.gameObject.CompareTag("Fire"))
+        {
+            transform.position = spawnPoint.transform.position;
+
+        }
+    }
+}
